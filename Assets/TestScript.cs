d160fb508e69f02a1f299d272761513551aac11f
André Cardoso <amc.acardoso@gmail.com>
2020-03-23 12:26:02 +0100

    fix sword direction

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -10,6 +10,7 @@ public class TestScript : MonoBehaviour
 {
     public VisualEffect hey;
     public Volume slowMotionVolume;
+    public Transform targets;
 
     // Start is called before the first frame update
     void Start()
@@ -36,5 +37,14 @@ public class TestScript : MonoBehaviour
     public void SlowmotionPostProcessing(float x)
     {
         slowMotionVolume.weight = x;
+    }
+
+    public void Sequence()
+    {
+        int ran = Random.Range(0, targets.childCount - 1);
+
+        Sequence s = DOTween.Sequence();
+        s.Append(transform.DOLookAt(targets.GetChild(ran).position, .35f));
+        s.Join(transform.DOMove(targets.GetChild(ran).position, .6f).SetEase(Ease.InCirc));
     }
 }
