316600ad078f5f5c2f2a640d05b7b368c900f71c
André Cardoso <amc.acardoso@gmail.com>
2020-03-25 23:47:18 +0100

    Camera Target

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -8,24 +8,63 @@ using DG.Tweening;
 
 public class TestScript : MonoBehaviour
 {
+    [Header("Targets in radius")]
+    public List<Transform> targets;
+    public int count;
+
+    [Space]
+
     public VisualEffect hey;
     public Volume slowMotionVolume;
-    public Transform targets;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject gameCam;
+    public GameObject targetCam;
 
-    // Update is called once per frame
+    public Transform aimObject;
+
+
     void Update()
     {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Time.timeScale = (Time.timeScale == 1) ? .05f : 1;
+        //    DOVirtual.Float(slowMotionVolume.weight, (slowMotionVolume.weight == 0) ? 1 : 0, .2f, SlowmotionPostProcessing).SetUpdate(true);
+        //}
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Time.timeScale = (Time.timeScale == 1) ? .05f : 1;
-            DOVirtual.Float(slowMotionVolume.weight, (slowMotionVolume.weight == 0) ? 1 : 0, .2f, SlowmotionPostProcessing).SetUpdate(true);
+            if (!targetCam.active)
+            {
+                Time.timeScale = .1f;
+                targetCam.SetActive(true);
+                DOVirtual.Float(0, 1, .3f, SlowmotionPostProcessing).SetUpdate(true);
+                LookAt();
+            }
+            else
+            {
+                DOVirtual.Float(1, 0, .3f, SlowmotionPostProcessing).SetUpdate(true);
+                Time.timeScale = 1;
+                targetCam.SetActive(false);
+            }
+
         }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            int direction = (Input.GetKey(KeyCode.RightArrow)) ? 1 : -1;
+            count += direction;
+            if (count > targets.Count - 1)
+                count = 0;
+            if (count < 0)
+                count = targets.Count - 1;
+            LookAt();
+
+        }
+    }
+
+    public void LookAt()
+    {
+        aimObject.DOLookAt(targets[count].position, .3f).SetUpdate(true);
     }
 
     public void PlayVFX()
@@ -34,17 +73,18 @@ public class TestScript : MonoBehaviour
         Camera.main.GetComponent<CinemachineImpulseSource>().GenerateImpulse();
     }
 
+
     public void SlowmotionPostProcessing(float x)
     {
         slowMotionVolume.weight = x;
     }
 
-    public void Sequence()
-    {
-        int ran = Random.Range(0, targets.childCount - 1);
 
-        Sequence s = DOTween.Sequence();
-        s.Append(transform.DOLookAt(targets.GetChild(ran).position, .35f));
-        s.Join(transform.DOMove(targets.GetChild(ran).position, .6f).SetEase(Ease.InCirc));
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            targets.Add(other.transform);
+        }
     }
 }
